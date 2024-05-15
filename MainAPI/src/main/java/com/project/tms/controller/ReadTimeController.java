eaac890c43da19f6a8e6bb460f4bfd76bb96a863
311516h <82697724+311516h@users.noreply.github.com>
2024-05-15 20:34:16 +0900

    #30 - 상위4개 get api 코드 수정
    상위4개 get api 코드 수정

--- MainAPI/src/main/java/com/project/tms/controller/ReadTimeController.java
+++ MainAPI/src/main/java/com/project/tms/controller/ReadTimeController.java
@@ -184,10 +184,8 @@ public class ReadTimeController {
     public ResponseEntity<Map<String, Object>> getTopCategoriesPercentage(@PathVariable Long memberId) {
         Map<String, Object> responseBody = new HashMap<>();
 
-        // 해당 memberId의 ReadTime 엔티티를 가져옵니다.
         List<ReadTime> readTimes = readTimeService.getReadTimesByMemberId(memberId);
 
-        // 각 카테고리의 총 읽은 시간을 계산합니다.
         Map<String, Long> categoryTotalTime = new HashMap<>();
         for (ReadTime readTime : readTimes) {
             String category = readTime.getCategory();
@@ -224,7 +222,6 @@ public class ReadTimeController {
             categoryTotalTime.put(category, totalTime);
         }
 
-        // 총 읽은 시간을 기준으로 백분율을 계산합니다.
         long totalReadTime = categoryTotalTime.values().stream().mapToLong(Long::longValue).sum();
         Map<String, Integer> categoryPercentages = new HashMap<>();
         for (Map.Entry<String, Long> entry : categoryTotalTime.entrySet()) {
@@ -232,13 +229,11 @@ public class ReadTimeController {
             categoryPercentages.put(entry.getKey(), percentage);
         }
 
-        // 백분율이 높은 상위 4개의 카테고리를 선택합니다.
         List<Map.Entry<String, Integer>> sortedPercentages = categoryPercentages.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(4)
                 .collect(Collectors.toList());
 
-        // 결과를 responseBody에 추가합니다.
         List<Map<String, Object>> topCategories = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : sortedPercentages) {
             Map<String, Object> categoryMap = new HashMap<>();
@@ -247,7 +242,6 @@ public class ReadTimeController {
             topCategories.add(categoryMap);
         }
 
-        // 데이터가 4개 미만인 경우 나머지를 null로 채웁니다.
         while (topCategories.size() < 4) {
             Map<String, Object> nullCategoryMap = new HashMap<>();
             nullCategoryMap.put("category", "null");
@@ -255,7 +249,6 @@ public class ReadTimeController {
             topCategories.add(nullCategoryMap);
         }
 
-        // memberId 정보를 추가합니다.
         responseBody.put("memberId", memberId);
         responseBody.put("topCategories", topCategories);
 
